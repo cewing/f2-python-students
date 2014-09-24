bde260ecc966b8eb40140690ef088170775d36d4
Chris Barker <PythonCHB@gmail.com>
2014-09-23 23:16:43 -0700

    fixed bug with dash in trigrams
    pep 8 for trigram solution

--- Solutions/session04/trigram_solution.py
+++ Solutions/session04/trigram_solution.py
@@ -29,14 +29,10 @@ def make_words(text):
     # there are other ways to do this:
     #   a_word.strip() works well, too.
 
-    # NOTE: I keep the single quotes, because it is also used as an
-    # apostophe -- but that does end up with some wierd results
-    # so you could be smarter about that.
-
     punctuation = unicode(string.punctuation)
-    string.punctuation.replace("'", "") # keep apostropies
-    string.punctuation.replace("-", "") # keep hyphenated words
-    table = dict([ (ord(c), None) for c in punctuation])
+    punctuation = string.punctuation.replace("'", "")  # keep apostropies
+    punctuation = string.punctuation.replace("-", "")  # keep hyphenated words
+    table = dict([(ord(c), None) for c in punctuation])
 
     # lower-case everything to remove that complication:
     text = text.lower()
@@ -72,7 +68,6 @@ def read_in_data(infilename):
     return u" ".join(full_text)
 
 
-
 def build_trigram(words):
     """build a trigram dict from the passed-in text"""
 
@@ -81,12 +76,11 @@ def build_trigram(words):
     # The values will be a list of the words that follow each pair
     word_pairs = {}
 
-
     # loop through the words
     for i in range(len(words) - 2):
-        pair = tuple( words[i:i+2] )  # a tuple so it can be a key in the dict
+        pair = tuple(words[i:i+2])  # a tuple so it can be a key in the dict
         follower = words[i+2]
-        word_pairs.setdefault(pair,[]).append(follower)
+        word_pairs.setdefault(pair, []).append(follower)
 
         # setdefault() returns the value if pair is already in the dict
         #    if it's not, it adds it, setting the value to a an empty list
@@ -99,6 +93,30 @@ def build_trigram(words):
     return word_pairs
 
 
+def build_text(word_pairs):
+
+    """build some new text from the word_pair dict supplied"""
+
+    new_text = []
+    for i in range(30):  # do thirty sentences
+        # pick a word pair to start the sentence
+        sentence = list(random.choice(word_pairs.keys()))
+
+        # now add a random number of additional words to the sentence
+        for j in range(random.randint(2, 10)):
+            pair = tuple(sentence[-2:])
+            sentence.append(random.choice(word_pairs[pair]))
+        # capitalize the first word:
+        sentence[0] = sentence[0].capitalize()
+        # Add the period
+        sentence[-1] += u"."
+        new_text.extend(sentence)
+
+    new_text = " ".join(new_text)
+
+    return new_text
+
+
 if __name__ == "__main__":
 
     # get the filename from the command line
@@ -108,26 +126,9 @@ if __name__ == "__main__":
         print "You must pass in a filename"
         sys.exit(1)
 
-
     in_data = read_in_data(filename)
     words = make_words(in_data)
     word_pairs = build_trigram(words)
-    # create some new text
-    new_text = []
-    for i in range(30): # do thirty sentences
-        # pick a word pair to start the sentence
-        sentence = list(random.choice( word_pairs.keys() ) )
-
-        # now add a random number of additional words to the sentence
-        for j in range(random.randint(2,10)):
-            pair = tuple(sentence[-2:])
-            sentence.append( random.choice(word_pairs[pair]) )
-        #capitalize the first word:
-        sentence[0] = sentence[0].capitalize()
-        #Add the period
-        sentence[-1] += u"."
-        new_text.extend(sentence)
-
-    new_text = " ".join(new_text)
+    new_text = build_text(word_pairs)
 
     print new_text
