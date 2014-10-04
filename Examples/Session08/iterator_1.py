c814985dfa3346fcf49fad006f92b1df8c53d33b
Chris Barker <PythonCHB@gmail.com>
2014-10-04 14:01:45 -0700

    updated examples a bit

--- Examples/Session08/iterator_1.py
+++ Examples/Session08/iterator_1.py
@@ -24,6 +24,26 @@ class IterateMe_1(object):
         else:
             raise StopIteration
 
+class IterateMe_2(object):
+    """
+    About as simple an iterator as you can get:
+
+    returns the sequence of numbers from zero to 4
+    ( like xrange(4) )
+    """
+    def __init__(self, stop=5):
+        self.current = -1
+        self.stop = stop
+    def __iter__(self):
+        return self
+    def next(self):
+        self.current += 1
+        if self.current < self.stop:
+            return self.current
+        else:
+            raise StopIteration
+
+
 
 if __name__ == "__main__":
 
